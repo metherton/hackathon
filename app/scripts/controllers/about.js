646e8b79a0bc0a237a0a2da55ec30612f2be3291
metherton <martinetherton63@gmail.com>
2016-03-14 21:58:27 +0100

    first commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name inghackathonclientApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the inghackathonclientApp
+ */
+angular.module('inghackathonclientApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name inghackathonclientApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the inghackathonclientApp
+ */
+angular.module('inghackathonclientApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
