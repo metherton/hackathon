09b950f84fa9cd6420373e2fb0b74308098fd59b
metherton <martinetherton63@gmail.com>
2016-03-14 23:39:12 +0100

    use ui . router

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -11,21 +11,56 @@
 angular
   .module('inghackathonclientApp', [
     'ngResource',
-    'ngRoute'
+    'ui.router'
   ])
-  .config(function ($routeProvider) {
-    $routeProvider
-      .when('/', {
-        templateUrl: 'views/main.html',
-        controller: 'MainCtrl',
-        controllerAs: 'main'
+  //.config(function () {
+    .config(function ($stateProvider, $urlRouterProvider) {
+    //$stateProvider
+    //  .when('/', {
+    //    templateUrl: 'views/main.html',
+    //    controller: 'MainCtrl',
+    //    controllerAs: 'main'
+    //  })
+    //  .when('/about', {
+    //    templateUrl: 'views/about.html',
+    //    controller: 'AboutCtrl',
+    //    controllerAs: 'about'
+    //  })
+    //  .otherwise({
+    //    redirectTo: '/'
+    //  });
+
+    $stateProvider
+
+    // route for the home page
+      .state('app', {
+        url:'/',
+        views: {
+          'header': {
+            templateUrl : 'views/header.html',
+          },
+          'content': {
+            templateUrl : 'views/main.html',
+            controller  : 'MainCtrl'
+          },
+          'footer': {
+            templateUrl : 'views/footer.html',
+          }
+        }
+
       })
-      .when('/about', {
-        templateUrl: 'views/about.html',
-        controller: 'AboutCtrl',
-        controllerAs: 'about'
-      })
-      .otherwise({
-        redirectTo: '/'
+
+      // route for the aboutus page
+      .state('app.aboutus', {
+        url:'aboutus',
+        views: {
+          'content@': {
+            templateUrl : 'views/about.html',
+            controller  : 'AboutCtrl'
+          }
+        }
       });
+
+    $urlRouterProvider.otherwise('/');
+
   });
