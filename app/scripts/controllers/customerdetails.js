d7c7183fc344f6faa18fdeae1a209634d79d8a1b
metherton <martinetherton63@gmail.com>
2016-03-18 22:24:26 +0100

    add customers

--- app/scripts/controllers/customerdetails.js
+++ app/scripts/controllers/customerdetails.js
@@ -8,26 +8,36 @@
  * Controller of the inghackathonclientApp
  */
 angular.module('inghackathonclientApp')
-  .controller('CustomerDetailsCtrl', ['smsFactory', 'feedbackFactory', function (smsFactory, feedbackFactory) {
+  .controller('CustomerDetailsCtrl', ['smsFactory', 'feedbackFactory', 'customerDetailsFactory', function (smsFactory, feedbackFactory, customerDetailsFactory) {
 
     var vm = this;
+
+    vm.chosenCustomerDetail = {};
 
     vm.smsMessage = {
       body: "",
       to: "0031624543741"
     };
 
-    vm.customerDetails = {
-      id: "1234567",
-      department: "ROSETTA",
-      channelType: "CALL",
-      corpKey: "DT10DW"
-    };
+    vm.customerDetails = [];
+
+    vm.customerDetails = customerDetailsFactory.query(function(response) {
+       vm.customerDetails = response;
+    }, function(error) {
+      console.log(error);
+    });
+
+    //vm.customerDetails = {
+    //  id: "1234567",
+    //  department: "ROSETTA",
+    //  channelType: "CALL",
+    //  corpKey: "DT10DW"
+    //};
 
     vm.sendFeedback = function() {
       console.log('send sms');
-      feedbackFactory.save(vm.customerDetails).$promise.then(function(feedbackResponse) {
-        vm.smsMessage.body += " " + feedbackResponse.url;
+      feedbackFactory.save(vm.chosenCustomerDetail).$promise.then(function(feedbackResponse) {
+        vm.smsMessage.body += " " + "Hi " + vm.chosenCustomerDetail.name + " please give us your feedback at " + feedbackResponse.url;
         return smsFactory.save(vm.smsMessage);
       }, function(error) {
         console.log('there is an error');
--- app/scripts/services/services.js
+++ app/scripts/services/services.js
@@ -4,13 +4,23 @@ angular.module('inghackathonclientApp.services', ['ngResource'])
   //.constant("baseURL","http://192.168.0.101:3000/")
   .constant("baseURL","http://localhost:8080/")
   .constant("feedbackURL","http://localhost:8080/")
+  .constant("customerDetailsURL","http://141.138.139.81:3000/")
   //.constant("baseURL","http://141.138.139.81:3000/")
-  .factory('feedbackFactory', ['$resource', 'feedbackURL', function($resource,baseURL) {
+  .factory('feedbackFactory', ['$resource', 'feedbackURL', function($resource,feedbackURL) {
     //  .
     //.factory('smsFactory', [function() {
 
 
-    return $resource(baseURL+"feedback/:id", {},
+    return $resource(feedbackURL+"feedback/:id", {},
+      {'save': {method: 'POST', headers: {'Content-Type':'application/json', 'Accept': 'application/json'} }});
+
+  }])
+  .factory('customerDetailsFactory', ['$resource', 'customerDetailsURL', function($resource,customerDetailsURL) {
+    //  .
+    //.factory('smsFactory', [function() {
+
+
+    return $resource(customerDetailsURL+"customerDetails/:id", {},
       {'save': {method: 'POST', headers: {'Content-Type':'application/json', 'Accept': 'application/json'} }});
 
   }])
