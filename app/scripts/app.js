896f8274e7cab909f312161564310359195cb477
DeVries1 <newfhenix@hotmail.com>
2016-05-05 04:01:07 +0200

    Update to single purpose app

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,12 +1,9 @@
 'use strict';
 
 /**
- * @ngdoc overview
- * @name inghackathonclientApp
- * @description
- * # inghackathonclientApp
- *
- * Main module of the application.
+ * @ngdoc 		overview
+ * @name 		inghackathonclientApp
+ * @description	Main module of the application.
  */
 angular
   .module('inghackathonclientApp', [
@@ -22,45 +19,8 @@ angular
     .state('app', {
       url:'/',
       views: {
-        'header': {
-          templateUrl : 'views/header.html',
-        },
-        'content': {
-          templateUrl : 'views/main.html',
-          controller  : 'MainCtrl'
-        },
-        'footer': {
-          templateUrl : 'views/footer.html',
-        }
-      }
-
-    })
-    // route for the aboutus page
-    .state('app.sms', {
-      url:'sms',
-      views: {
-        'content@': {
-          templateUrl : 'views/sms.html',
-          controller  : 'SmsCtrl'
-        }
-      }
-    })
-    .state('app.customerdetails', {
-      url:'customerdetails',
-      views: {
         'content@': {
           templateUrl : 'views/customerdetails.html',
-          controller  : 'CustomerDetailsCtrl',
-          controllerAs : 'vm'
-        }
-      }
-    })
-    // route for the aboutus page
-    .state('app.aboutus', {
-      url:'aboutus',
-      views: {
-        'content@': {
-          templateUrl : 'views/about.html',
           controller  : 'CustomerDetailsCtrl',
           controllerAs : 'vm'
         }
--- app/scripts/controllers/customerdetails.js
+++ app/scripts/controllers/customerdetails.js
@@ -1,201 +1,82 @@
 'use strict';
 
 /**
- * @ngdoc function
- * @name inghackathonclientApp.controller:AboutCtrl
- * @description
- * # AboutCtrl
  * Controller of the inghackathonclientApp
  */
 angular.module('inghackathonclientApp')
-  .controller('CustomerDetailsCtrl', ['smsFactory', 'bulkSmsFactory','feedbackFactory', 'customerDetailsFactory', function (smsFactory, bulkSmsFactory, feedbackFactory, customerDetailsFactory) {
+  .controller('CustomerDetailsCtrl', ['smsFactory','feedbackFactory', function (smsFactory, feedbackFactory) {
 
     var vm = this;
 
-    //vm.groups = [
-    //  {name: "leeuwarden",
-    //    id: 8},
-    //  {name: "developers",
-    //    id: 9},
-    //  {name: "rutger",
-    //    id: 10},
-    //  {name: "twilio",
-    //    id: 11},
-    //  {name: "TestGroup",
-    //    id: 1},
-    //  {name: "SMILingDX",
-    //    id: 2},
-    //  {name: "Subjury1",
-    //    id: 3},
-    //  {name: "Subjury2",
-    //    id: 4},
-    //  {name: "Subjury3",
-    //    id: 5},
-    //  {name: "24hCodING",
-    //  id: 6},
-    //  {name: "ExtraGroup",
-    //    id: 7}
-    //];
+	function initData(){
+		// customer data
+		vm.customerData = {};
+		vm.customerData.department = "Klantfeedback";
+		vm.customerData.question = "Je hebt een ING kantoor bezocht";	
+		vm.customerData.gender = "";
+		vm.customerData.ageBracket = "";
+		vm.customerData.privateData = {};
+		vm.customerData.privateData.name = "";
+		vm.customerData.privateData.telephone = "";
 
-    vm.groups = [
-      {name: "hypotheken",
-        id: 1},
-      {name: "demo",
-        id: 2}
-    ];
-
-    vm.chosenCustomerDetail = {};
-
-    //vm.chosenCustomerDetail.department = "Hoofddorp";
-    //vm.chosenCustomerDetail.question = "You visited an ING branch office";
-    //
-    //vm.chosenCustomerDetail.department = "24hCodING";
-    //vm.chosenCustomerDetail.question = "You participated in the 24hCodING";
-
-    vm.chosenCustomerDetail.department = "hypotheken";
-    vm.chosenCustomerDetail.question = "You visited the quarterly meeting";
-
-    vm.smsMessage = {
-      message: "",
-      to: "+31624543741"
-    };
-
-    vm.customerDetails = [];
-
-    vm.customerDetails = customerDetailsFactory.query(function(response) {
-       vm.customerDetails = response;
-      console.log(vm.customerDetails);
-    }, function(error) {
-      console.log(error + "ho");
-    });
-
+		vm.smsMessage = {
+		  message: "",
+		  to: ""
+		};
+	}
+	
+	initData();
+	
+	vm.resultData = "";
+	vm.busy = false;
 
     vm.sendFeedback = function(alsoSendSms) {
-      vm.chosenCustomerDetail.to = vm.chosenCustomerDetail.customerObject.telephone;
-      vm.chosenCustomerDetail.name = vm.chosenCustomerDetail.customerObject.name;
-
-      //vm.smsMessage.to = vm.chosenCustomerDetail.to
-
+	  vm.busy = true;
+	  
       var newCustomer = {};
 
-      newCustomer.customerId = vm.chosenCustomerDetail.name;
-      newCustomer.to = vm.chosenCustomerDetail.to;
-      newCustomer.departmentId = vm.chosenCustomerDetail.department;
-      newCustomer.question = vm.chosenCustomerDetail.question;
+      newCustomer.customerId = vm.customerData.gender;
+      newCustomer.to = vm.customerData.ageBracket;
+      newCustomer.departmentId = vm.customerData.department;
+      newCustomer.question = vm.customerData.question;
 
+	  var customerName = vm.customerData.privateData.name;
+	  
+	  vm.smsMessage.to = vm.customerData.privateData.telephone;
+	  
       feedbackFactory.save(newCustomer).$promise.then(function(feedbackResponse) {
         console.log(feedbackResponse);
-        //vm.smsMessage.message = "Hello " + newCustomer.customerId + ", could you provide feedback? Please use: " + feedbackResponse.link + " Regards, ING ";
-        vm.smsMessage.message = "Dear " + newCustomer.customerId + ", you visited the hypotheken tribe meeting. Leave your smile: " + feedbackResponse.link;
-
+        vm.smsMessage.message = "Hallo " + customerName + ", je hebt een ING kantoor bezocht. Geef je feedback via: " + feedbackResponse.link;
+		
         if(alsoSendSms) {
-          vm.sendSms();
-        }
+          vm.sendSms(true);
+        } else {
+		  vm.busy = false;
+		}
+		vm.resultData = "Feedback aangevraagd... ";
       }, function(error) {
         console.log('there is an error with the feedback api.');
+		vm.resultData = "Mislukt! Het is niet gelukt om de feedback te registreren...";
+		vm.busy = false;
       });
     };
 
-    vm.sendSms = function() {
-      vm.smsMessage.to = vm.chosenCustomerDetail.to;
-      smsFactory.save(vm.smsMessage);
-    };
-
-    vm.sendSmsBulk = function() {
-      bulkSmsFactory.save(bulkSmsArray).$promise.then(function(response) {
-        var nrOfMessagesSent = 0;
-        for(var i=0; i< response.length; i++){
-          if(response[i].status == "OK") {
-            nrOfMessagesSent++;
-          }
-        }
-        vm.bulkResultText = " " + nrOfMessagesSent + " messages sent";
+    vm.sendSms = function(phoneset) {
+	  vm.busy = true;
+	  
+	  if(!phoneset){
+		vm.smsMessage.to = vm.customerData.privateData.telephone;			
+	  }
+      smsFactory.save(vm.smsMessage).$promise.then(function(response) {
+		console.log('SMS verzonden');
+		initData();
+		vm.resultData += "en SMS succesvol verzonden.";
+		vm.busy = false;
+      }, function(error) {
+        console.log('there is an error with the sms api.');
+		vm.resultData = "Mislukt! Het is niet gelukt de SMS te verzenden...";
+		vm.busy = false;
       });
-      vm.bulkSmsRecipients = "";
     };
-
-    var bulkSmsArray;
-    var nrOfFeedbackRequests;
-
-    vm.processAllCustomers = function(){
-      nrOfFeedbackRequests = 0;
-
-      for(var i=0; i< vm.customerDetails.length; i++){
-        if(vm.chosenGroup.name == vm.customerDetails[i].group){
-          nrOfFeedbackRequests++;
-        }
-      }
-
-      if (vm.customerDetails.length > 0) {
-        //start
-        bulkSmsArray = [];
-        vm.bulkSmsRecipients = "";
-        vm.processNextCustomer(0);
-      }
-    }
-
-    vm.processNextCustomer = function(id){
-      var newCustomer = {};
-
-      newCustomer.customerId = vm.customerDetails[id].name;
-      newCustomer.to = vm.customerDetails[id].telephone;
-      newCustomer.departmentId = vm.chosenCustomerDetail.department;
-      newCustomer.question = vm.chosenCustomerDetail.question;
-
-      var correctGroup = false;
-
-      if(vm.chosenGroup.name == vm.customerDetails[id].group){
-        // correctGroup = true;
-        feedbackFactory.save(newCustomer).$promise.then(succesFunc(newCustomer), errorFunc(newCustomer)).finally(finallyFunc(id));
-      } else {
-        //correctGroup = false;
-        finallyFunc(id)();
-      }
-    }
-
-    function succesFunc(newCustomer){
-      return function(feedbackResponse) {
-        var newSms = {};
-
-        newSms.to = newCustomer.to;
-        newSms.message = "Dear " + newCustomer.customerId + ", you visited the hypotheken tribe meeting. Leave your smile: " + feedbackResponse.link;
-        bulkSmsArray.push(newSms);
-      }
-    }
-
-    function errorFunc(newCustomer) {
-      return function (error) {
-        console.log('Skipping customer: ' + newCustomer);
-      }
-    }
-
-    function finallyFunc(id) {
-      return function(){
-        var nextId = id + 1;
-        vm.bulkProcessingText = " Busy with retrieving feedback link " + bulkSmsArray.length + " of " + nrOfFeedbackRequests + "...";
-
-        //// Temp to bombard
-        //if(nextId == vm.customerDetails.length && vm.bombard > 0){
-        //  vm.bombard--;
-        //  nextId = 0;
-        //}
-
-        if (nextId < vm.customerDetails.length) {
-          vm.processNextCustomer(nextId);
-        } else if(nextId == vm.customerDetails.length){
-          // end of the line, send sms
-          console.log('Result');
-          vm.bulkSmsRecipients = "";
-          for(var i=0; i< bulkSmsArray.length; i++){
-            vm.bulkSmsRecipients += bulkSmsArray[i].to + "\n";
-            //console.log('Object: ' + bulkSmsArray[i].message);
-          }
-
-          vm.bulkProcessingText = " Processed feedback link for " + bulkSmsArray.length + " of " + nrOfFeedbackRequests + "...";
-        }
-      }
-    }
-
-
 
   }]);
